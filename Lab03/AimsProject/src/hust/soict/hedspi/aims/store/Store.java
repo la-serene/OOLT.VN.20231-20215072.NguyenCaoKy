6fae8bd2f99f46a65a2b239ae8adb38afb3952df
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-23 20:14:31 +0700

    update store

--- Lab03/AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ Lab03/AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,4 +1,14 @@
 package hust.soict.hedspi.aims.store;
 
+import hust.soict.hedspi.aims.disc.DVD;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
+    List<DVD> itemsInStore = new ArrayList<>();
+
+    public void addDVD(DVD dvd) {
+        this.itemsInStore.add(dvd);
+    }
 }
