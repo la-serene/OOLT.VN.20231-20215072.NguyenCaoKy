479b3b8b2b015179d6cd0cb3a5f019b644d1ffda
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-23 17:24:13 +0700

    aims re-organize

---
+++ Lab03/AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -0,0 +1,21 @@
+package hust.soict.hedspi.test.cart;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.disc.DVD;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DVD dvd1 = new DVD("The Lion King", "Animation", "Rogers Alter", 87, 19.95f);
+        cart.addDVDsToCart(dvd1);
+
+        DVD dvd2 = new DVD("Inception", "Sci-Fi", "Christopher Nolan", 148, 24.99f);
+        cart.addDVDsToCart(dvd2);
+
+        DVD dvd3 = new DVD("Casablanca", "Drama", "Michael Curtiz", 102, 14.99f);
+        cart.addDVDsToCart(dvd3);
+
+        cart.print();
+    }
+}
