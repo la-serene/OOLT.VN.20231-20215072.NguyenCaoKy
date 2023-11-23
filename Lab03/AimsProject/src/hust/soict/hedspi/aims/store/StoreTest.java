bd0cfd130f4fa829964f7344613bfb478e0c03a1
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-23 20:41:48 +0700

    update StoreTest

---
+++ Lab03/AimsProject/src/hust/soict/hedspi/aims/store/StoreTest.java
@@ -0,0 +1,17 @@
+package hust.soict.hedspi.aims.store;
+
+import hust.soict.hedspi.aims.disc.DVD;
+public class StoreTest {
+    public static void main(String[] args) {
+        Store s = new Store();
+
+        DVD dvd1 = new DVD("The Lion King", "Animation", "Rogers Alter", 87, 19.95f);
+        s.addDVDtoStore(dvd1);
+
+        DVD dvd2 = new DVD("Inception", "Sci-Fi", "Christopher Nolan", 148, 24.99f);
+        s.addDVDtoStore(dvd2);
+
+        DVD dvd3 = new DVD("Casablanca", "Drama", "Michael Curtiz", 102, 14.99f);
+        s.addDVDtoStore(dvd3);
+    }
+}
