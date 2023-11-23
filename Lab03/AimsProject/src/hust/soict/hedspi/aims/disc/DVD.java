4ee78aba0a3911bfd9c731af6a13aee01dca9336
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-23 20:42:25 +0700

    update DVD

--- Lab03/AimsProject/src/hust/soict/hedspi/aims/disc/DVD.java
+++ Lab03/AimsProject/src/hust/soict/hedspi/aims/disc/DVD.java
@@ -2,7 +2,7 @@ package hust.soict.hedspi.aims.disc;
 
 public class DVD {
     private static int nbDigitalVideoDiscs = 0;
-    private int id;
+    private final int id;
     private String title;
     private String category;
     private String director;
@@ -13,12 +13,14 @@ public class DVD {
         this.id = ++nbDigitalVideoDiscs;
         this.title = title;
     }
+
     public DVD(String category, String title, float cost) {
         this.id = ++nbDigitalVideoDiscs;
         this.category = category;
         this.title = title;
         this.cost = cost;
     }
+
     public DVD(String director, String category, String title, float cost) {
         this.id = ++nbDigitalVideoDiscs;
         this.director = director;
@@ -26,6 +28,7 @@ public class DVD {
         this.title = title;
         this.cost = cost;
     }
+
     public DVD(String title, String category, String director, int length, float cost) {
         this.id = ++nbDigitalVideoDiscs;
         this.title = title;
@@ -35,29 +38,39 @@ public class DVD {
         this.cost = cost;
     }
 
-    public int getId() { return id; }
+    public int getId() {
+        return id;
+    }
+
     public String getTitle() {
         return title;
     }
+
     public String getCategory() {
         return category;
     }
+
     public String getDirector() {
         return director;
     }
+
     public int getLength() {
         return length;
     }
+
     public float getCost() {
         return cost;
     }
-    public void setTitle(String title) {this.title = title;}
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String toString() {
-        return String.format("DVD - %s - %s - %s - %d: %f $%n", this.getTitle(), this.getCategory(), this.getDirector(), this.getLength(), this.getCost());
+        return String.format("DVD - %s - %s - %s - %d: %.2f $%n", this.getTitle(), this.getCategory(), this.getDirector(), this.getLength(), this.getCost());
     }
 
     public boolean isMatch(String title) {
-        return this.title.equals(title);
+        return this.getTitle().equals(title);
     }
 }
