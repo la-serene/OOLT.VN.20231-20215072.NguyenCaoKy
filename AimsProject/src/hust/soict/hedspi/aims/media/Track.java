e5aec923caf4c89d458e1e77417424b72e1e5c54
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-24 22:19:14 +0700

    add Track cls

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Track.java
@@ -0,0 +1,13 @@
+package hust.soict.hedspi.aims.media;
+
+public class Track {
+    private String title;
+    private int length;
+    public Track() {};
+    public String getTitle() {
+        return title;
+    }
+    public int getLength() {
+        return length;
+    }
+}
