33e901583b2ef641626b175a7146c544a69ab2cc
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-25 01:11:48 +0700

    adapt media to store

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,20 +1,27 @@
 package hust.soict.hedspi.aims.store;
 
 import hust.soict.hedspi.aims.media.DVD;
+import hust.soict.hedspi.aims.media.Media;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
-    List<DVD> itemsInStore = new ArrayList<>();
-
-    public void addDVDtoStore(DVD dvd) {
-        System.out.println("New DVD added!");
-        this.itemsInStore.add(dvd);
+    public ArrayList<Media> itemsInStore = new ArrayList<>();
+    public void addMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            System.out.println("Media already exists!");
+        } else {
+            itemsInStore.add(media);
+            System.out.println("Media added!");
+        }
     }
-
-    public void removeDVDtoStore(DVD dvd) {
-        System.out.println("DVD successfully removed!");
-        this.itemsInStore.remove(dvd);
+    public void removeMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            itemsInStore.remove(media);
+            System.out.println("Media removed!");
+        } else {
+            System.out.println("Media not found!");
+        }
     }
 }
