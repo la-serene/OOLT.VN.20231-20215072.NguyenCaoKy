86e78a20b70a727a4fd15409d40d5ce0ab6769e5
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-24 21:53:04 +0700

    add Book cls

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Book.java
@@ -0,0 +1,16 @@
+package hust.soict.hedspi.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private final List<String> authors = new ArrayList<>();
+
+    public Book() {
+
+    }
+}
