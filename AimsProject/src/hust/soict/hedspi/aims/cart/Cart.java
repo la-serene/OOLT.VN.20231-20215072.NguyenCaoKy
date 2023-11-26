11cb2002625de387d298ffb666a571b8de347521
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-26 22:49:45 +0700

    remove unnecessary methods

--- AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
@@ -1,11 +1,8 @@
 package hust.soict.hedspi.aims.cart;
 
-import hust.soict.hedspi.aims.media.DVD;
 import hust.soict.hedspi.aims.media.Media;
 
-import javax.swing.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Cart {
     public ArrayList<Media> itemsOrdered = new ArrayList<>();
@@ -38,30 +35,5 @@ public class Cart {
         for (int i = 0; i < itemsOrdered.size(); i++) {
             System.out.printf("%d. %s", i + 1, itemsOrdered.get(i).toString());
         }
-    }
-
-    public Media searchDVD(int id) {
-        for (int i = 0; i < itemsOrdered.size(); i++) {
-            if (id == itemsOrdered.get(i).getId()) {
-                System.out.printf("%d. %s", i + 1, itemsOrdered.get(i).toString());
-                
-                return itemsOrdered.get(i);
-            }
-        }
-        JOptionPane.showMessageDialog(null, "No matching found!");
-        return null;
-    }
-
-    public Media searchDVD(String title) {
-        for (int i = 0; i < itemsOrdered.size(); i++) {
-            String s = itemsOrdered.get(i).getTitle();
-            if (itemsOrdered.get(i).isMatch(title)) {
-                System.out.printf("%d. %s", i + 1, itemsOrdered.get(i).toString());
-
-                return itemsOrdered.get(i);
-            }
-        }
-        JOptionPane.showMessageDialog(null, "No matching found!");
-        return null;
     }
 }
