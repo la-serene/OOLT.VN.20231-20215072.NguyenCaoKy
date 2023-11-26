760ae5fdeae11a56c23829ae5ecf405be55b0a3e
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-26 22:22:02 +0700

    remove unnecessary attr

--- AimsProject/src/hust/soict/hedspi/aims/media/DVD.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/DVD.java
@@ -1,16 +1,7 @@
 package hust.soict.hedspi.aims.media;
 
 public class DVD extends Disc {
-    private static int nbDigitalVideoDiscs = 0;
-    private String director;
-    private int length;
     public DVD() { super(); }
-    public String getDirector() {
-        return director;
-    }
-    public int getLength() {
-        return length;
-    }
     public String toString() {
         return String.format("DVD - %s - %s - %s - %d: %.2f $%n", this.getTitle(), this.getCategory(), this.getDirector(), this.getLength(), this.getCost());
     }
