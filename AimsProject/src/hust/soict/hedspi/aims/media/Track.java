8f0a53cf084798bdb44b5188ab3d1f9d3feebf5b
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-11-26 22:24:00 +0700

    add Track constructor

--- AimsProject/src/hust/soict/hedspi/aims/media/Track.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Track.java
@@ -1,6 +1,15 @@
 package hust.soict.hedspi.aims.media;
 
 public record Track(String title, int length) implements Playable {
+    public Track(String title, int length) {
+        if (title.isBlank()) {
+            throw new IllegalArgumentException("Title cannot be blank!");
+        } else this.title = title;
+
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be positive!");
+        } else this.length = length;
+    }
     public boolean equals(Object o) {
         if (o instanceof Track track) {
             return this.title().equals(track.title()) && this.length() == track.length();
