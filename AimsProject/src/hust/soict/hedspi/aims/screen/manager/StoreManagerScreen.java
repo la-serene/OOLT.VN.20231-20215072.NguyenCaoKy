83536aa8748cdb14d610bcd66e232363a89f154a
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-12-03 15:43:37 +0700

    testing

--- AimsProject/src/hust/soict/hedspi/aims/screen/manager/StoreManagerScreen.java
+++ AimsProject/src/hust/soict/hedspi/aims/screen/manager/StoreManagerScreen.java
@@ -76,7 +76,7 @@ public class StoreManagerScreen extends JFrame {
         center.setLayout(new GridLayout(3, 3, 2, 2));
 
         ArrayList<Media> mediaInStore = store.getItemsInStore();
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < mediaInStore.size() || i < 9; i++) {
             MediaStore cell = new MediaStore(mediaInStore.get(i));
             center.add(cell);
         }
