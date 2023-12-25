856787c53123f181bd4b3feb7a77490f36a88ca4
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-12-25 13:53:05 +0700

    fix cart controller

---
+++ AimsProject/src/module-info.java
@@ -0,0 +1,10 @@
+module AimsProject {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.desktop;
+
+    opens hust.soict.hedspi.aims;
+    opens hust.soict.hedspi.test.screen.customer.store;
+}
